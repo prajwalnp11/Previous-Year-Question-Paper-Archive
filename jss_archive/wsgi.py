620fd4029218af9864675b443193f7feb8d9e501
prajwalnp11 <prajwalnp9999@gmail.com>
2026-07-02 18:50:18 +0530

    Add vercel.json, build.sh, and wsgi app entry point for Vercel deployment support

--- jss_archive/wsgi.py
+++ jss_archive/wsgi.py
@@ -3,3 +3,5 @@ import os
 from django.core.wsgi import get_wsgi_application
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'jss_archive.settings')
 application = get_wsgi_application()
+app = application
+
